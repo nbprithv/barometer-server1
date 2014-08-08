c5f2ec1be4d33a689166b4176d248b12825640de
Niranjan B Prithviraj <niranjan.prithviraj@gmail.com>
2014-08-08 12:07:02 -0700

    adding support for calculating distances

--- routes/place.js
+++ routes/place.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var geolib = require('geolib');
 var router = express.Router();
 
 var place = function (req, res) {
@@ -6,6 +7,15 @@ var place = function (req, res) {
 
 place.prototype.getPlace = function (req, res) {
     var places = require('../places.json');
+    for (var i = 0; i < places.results.length; i++) {
+        var distance = geolib.getDistance({latitude: 37.78583400, longitude: -122.40641700},
+                                {latitude: places.results[i].latitude, longitude: places.results[i].longitude}, 10);
+        places.results[i].distance_km = Math.round(distance * 0.001 * 10) / 10;
+
+        distance = Math.round(distance * 0.00062 * 10) / 10;
+        places.results[i].distance_miles = distance;
+    }
+    "37.78583400 -122.40641700";
     res.send(places);
 };
 
